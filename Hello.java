c904ff443d9d0a4d792206acdb3003f7589952fd
Atish Jain <atish_jain@yahoo.com>
2024-03-08 21:55:23 +0530

    Add files via upload
    Committed in 8th March 24

---
+++ Hello.java
@@ -0,0 +1,17 @@
+
+/* Program name: Hello.java
+   Task: Displays Welcome Message
+   Date: 08-March-24
+   Author: Atish Jain
+   */
+
+
+class Hello
+{
+	//main method is the starting point of the program
+	public static void main(String args[])
+	{
+		System.out.println("Hello Welcome to World of Java!");
+		System.out.print("Thank You!");
+	}
+}
