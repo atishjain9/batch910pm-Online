ebb7acfc61eac1305153f0c72d702a14edd82b6b
Atish Jain <atish_jain@yahoo.com>
2024-03-11 21:37:44 +0530

    Add files via upload
    Committed in 11 mar 24

---
+++ Add2.java
@@ -0,0 +1,13 @@
+//Program to Add 2 nos
+
+class Add2
+{
+	public static void main(String args[])
+	{
+		byte a=10,b=20,c=0;
+
+		c =(byte)(a+b);
+
+		System.out.println("Result is:"+c);
+	}
+}
---
+++ DataTypeDemo.java
@@ -0,0 +1,28 @@
+//Program to Implement all the datatypes in java
+
+class DataTypeDemo
+{
+	public static void main(String args[])
+	{
+		int rollno = 1001;
+		String name = "Raj";
+		char gender='M';
+		long mno = 9949128444L;
+		float height = 5.6f;
+		double  avg= 99.89;		
+		boolean isFeesPaid = false;
+
+
+		System.out.println("Rollno: "+rollno);
+		System.out.println("Name: "+name);
+		System.out.println("Gender: "+gender);
+		System.out.println("Mobile no: "+mno);
+		System.out.println("Height: "+height);
+		System.out.println("Average: "+avg);
+		System.out.println("Is Fees Paid: "+isFeesPaid);
+
+
+		
+
+	}
+}
